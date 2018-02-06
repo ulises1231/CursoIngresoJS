3a017c15f315b4f4fff388bd9996e7120aba496b
ulises1231 <36174467+ulises1231@users.noreply.github.com>
2018-02-06 19:45:45 -0300

    Exercises 6, 7 y 8
    parseInt y uso de operaciones aritmeticas

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,65 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var resultado;
+    var numeroUno;
+    var numeroDos;	
+
+    numeroUno= document.getElementById('numeroUno').value;
+    numeroDos=document.getElementById('numeroDos').value;
+    
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+
+    resultado=numeroUno+numeroDos;
+    alert(resultado);
 }
 
 function restar()
 {
-	
+	var resultado;
+    var numeroUno;
+    var numeroDos;	
+
+    numeroUno= document.getElementById('numeroUno').value;
+    numeroDos=document.getElementById('numeroDos').value;
+    
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+
+    resultado=numeroUno-numeroDos;
+    alert(resultado);
 }
 
 function multiplicar()
 { 
-	
+    var resultado;
+    var numeroUno;
+    var numeroDos;	
+
+    numeroUno= document.getElementById('numeroUno').value;
+    numeroDos=document.getElementById('numeroDos').value;
+    
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+    
+    resultado=numeroUno*numeroDos;
+    alert(resultado);
 }
 
 function dividir()
 {
-	
+		var resultado;
+    var numeroUno;
+    var numeroDos;	
+
+    numeroUno= document.getElementById('numeroUno').value;
+    numeroDos=document.getElementById('numeroDos').value;
+    
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+    
+    resultado=numeroUno/numeroDos;
+    alert(resultado);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,10 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+ 	var resultado;
+ 	resultado= 10%2;
+ 	resultado= 2%10;
+ 	alert(resultado);
+ 	 resultado=2%10;
+ 	 alert(resultado);
 }
